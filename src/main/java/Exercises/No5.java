68ab6ac610c6394564cb9aeef92b71791b2a44a0
jiahao <jiahao@mananacare.com>
2023-03-29 10:06:02 +0800

    No5&No344提交,优化代码

--- src/main/java/Exercises/No5.java
+++ src/main/java/Exercises/No5.java
@@ -5,6 +5,12 @@ package Exercises;
  * @Date: 2023/03/28/11:06
  * @Description:
  */
+
+/**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ *
+ * 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+ */
 public class No5 {
     public static String longestPalindrome(String s) {
         String res = "";
